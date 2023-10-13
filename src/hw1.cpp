4cd2d7de51c133989e1a42f55fb7aedba61b0452
vanvansan <103155715+vanvansan@users.noreply.github.com>
2023-10-13 15:14:55 -0700

    1_3 done

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -8,6 +8,23 @@ bool inCircle(Real x, Real y, Vector2 center, Real radius){
     return sqrt(pow(x - center.x, 2) + pow(y - center.y, 2)) < radius; // distance formula
 }
 
+bool inRectangle(Real x, Real y, Vector2 p_min, Vector2 p_max){
+    return (x <= p_max.x && y <= p_max.y) && (x >= p_min.x && y >= p_min.y);
+}
+
+//return true if positive half otherwise false
+bool halfPlane (Vector2 p0, Vector2 p1, Vector2 q){
+    Vector2 p01 = p1 - p0;
+    Vector2 n01 = Vector2{p01.y, -p01.x};
+    return dot((q - p0), n01) <= 0;
+}
+
+bool inTriangle(Real x, Real y, Vector2 p0, Vector2 p1, Vector2 p2){
+    Vector2 q = Vector2{x, y};
+    return halfPlane(p0, p1, q) &&  halfPlane(p1, p2, q) &&  halfPlane(p2, p0, q);
+}
+
+
 void paintCanvas(Image3* imgPtr,Real width, Real height, Vector3 color){
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
@@ -74,6 +91,7 @@ Image3 hw_1_2(const std::vector<std::string> &params) {
         Vector2 center = circle.center;
         Real radius = circle.radius;
         Vector3 color = circle.color;
+        // bounding box
         for (int y = center.y - radius; y < center.y + radius; y++){
             for (int x = center.x - radius; x < center.x + radius; x++){
                 if(x >= 0 && y >= 0 && inCircle(x,y,center,radius)){
@@ -82,9 +100,9 @@ Image3 hw_1_2(const std::vector<std::string> &params) {
             }
         }
     }
-
      return img;
 }
+
 
 Image3 hw_1_3(const std::vector<std::string> &params) {
     // Homework 1.3: render multiple shapes
@@ -94,12 +112,31 @@ Image3 hw_1_3(const std::vector<std::string> &params) {
 
     Scene scene = parse_scene(params[0]);
     std::cout << scene << std::endl;
-
+    std::vector<Shape> shapes = scene.shapes;
+    
     Image3 img(scene.resolution.x, scene.resolution.y);
+    paintCanvas(&img,scene.resolution.x, scene.resolution.y, scene.background);
+
 
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
-            img(x, y) = Vector3{1, 1, 1};
+            for (auto shape : shapes){
+                if (auto *circle = std::get_if<Circle>(&shape)) {
+                    if (inCircle(x, y, circle->center, circle->radius)){
+                        img(x,y) = circle->color;
+                    }
+                } 
+                else if (auto *rectangle = std::get_if<Rectangle>(&shape)) {
+                    if (inRectangle(x, y, rectangle->p_min, rectangle->p_max)){
+                        img(x, y) = rectangle->color;
+                    }
+                }
+                else if (auto *triangle = std::get_if<Triangle>(&shape)) {
+                    if (inTriangle(x, y, triangle->p0, triangle->p1, triangle->p2)){
+                        img(x, y) = triangle->color;
+                    }
+                }
+            }
         }
     }
     return img;
@@ -116,9 +153,10 @@ Image3 hw_1_4(const std::vector<std::string> &params) {
 
     Image3 img(scene.resolution.x, scene.resolution.y);
 
+
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
-            img(x, y) = Vector3{1, 1, 1};
+            //img(x, y) = Vector3{1, 1, 1};
         }
     }
     return img;
