ff0b2402deed3e86d2cf028b7c75788d21978cbd
vanvansan <103155715+vanvansan@users.noreply.github.com>
2023-11-10 13:59:59 -0800

    3_1 done

--- src/hw3.cpp
+++ src/hw3.cpp
@@ -5,8 +5,52 @@
 
 using namespace hw3;
 
+const int SCR_HEIGHT = 720;
+const int SCR_WIDTH = 1280;
+
+void resize_window(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}  
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 void hw_3_1(const std::vector<std::string> &params) {
-    // HW 3.1: Open a window using GLFW
+    // initialize
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    // create window obj
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH , SCR_HEIGHT, "hw window", NULL, NULL);
+
+    if (window == NULL){
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+    // assign method pointer
+    glfwSetFramebufferSizeCallback(window, resize_window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    }   
+    while(!glfwWindowShouldClose(window)){
+        // input
+        // processInput(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+    glfwTerminate();
 }
 
 void hw_3_2(const std::vector<std::string> &params) {
